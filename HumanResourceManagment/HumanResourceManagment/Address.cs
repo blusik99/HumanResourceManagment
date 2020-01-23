e4e59c7bbb4f0bbc8d1a0c49dbd94bfc10e99215
blusik99 <blusik999@outlook.com>
2020-01-23 21:31:13 +0100

    Inital commit

---
+++ HumanResourceManagment/HumanResourceManagment/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace HumanResourceManagment
+{
+    class Address
+    {
+        public string sStreet { get; set; }
+        public int iHouseNumber { get; set; }
+        public string sPostalAddress { get; set; }
+        public string sCity { get; set; }
+
+        public Address(string sStreet, int iHouseNumber, string sPostalAddress, string sCity)
+        {
+            this.sStreet = sStreet;
+            this.iHouseNumber = iHouseNumber;
+            this.sPostalAddress = sPostalAddress;
+            this.sCity = sCity;
+        }
+    }
+}
